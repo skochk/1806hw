62e7f3cc8a7811db60cd7bc03fa1a5ba5e3a342d
Sergey <kochkares@gmail.com>
2019-06-25 14:33:07 +0300

    v3

--- scripts/main.js
+++ scripts/main.js
@@ -1,44 +1,48 @@
 let a;
 let b;
 
-document.querySelector('.firstI').addEventListener('input',function(){
- a = +document.querySelector('.firstI').value;
-})
-
-document.querySelector('.secondI').addEventListener('input',function(){
- b = +document.querySelector('.secondI').value;
-})
 
 let calculator = {
-  sum: function(x,y){
-    return x+y;
+  firstValue: a,
+  secondValue: b,
+  sum: function(){
+    return this.firstValue + this.secondValue;
   },
-  minus: function(x,y){
-    return x-y;
+  minus: function(){
+    return this.firstValue - this.secondValue;
   },
-  mult: function(x,y){
-    return x*y;
+  mult: function(){
+    return this.firstValue * this.secondValue;
   }
 };
 
+document.querySelector('.firstI').addEventListener('input',function(){
+  calculator.firstValue = +document.querySelector('.firstI').value;
+})
+
+document.querySelector('.secondI').addEventListener('input',function(){
+  calculator.secondValue = +document.querySelector('.secondI').value;
+})
+
+
 document.querySelector('.sum').addEventListener('click', function(){
     
-  document.querySelector('textarea').innerText = calculator.sum(a,b);
-  document.querySelector('.firstI').value = calculator.sum(a,b);
+  document.querySelector('textarea').innerText = calculator.sum();
+  document.querySelector('.firstI').value = calculator.sum();
   document.querySelector('.secondI').value = '';
 })
 
 document.querySelector('.minus').addEventListener('click', function(){
   // calculator.minus();
-  document.querySelector('textarea').innerText = calculator.minus(a,b);
-  document.querySelector('.firstI').value = calculator.minus(a,b);
+  document.querySelector('textarea').innerText = calculator.minus();
+  document.querySelector('.firstI').value = calculator.minus();
   document.querySelector('.secondI').value = '';
 })
 
 document.querySelector('.mult').addEventListener('click', function(){
   // calculator.mult();
-  document.querySelector('textarea').innerText = calculator.mult(a,b);
-  document.querySelector('.firstI').value = calculator.mult(a,b);
+  document.querySelector('textarea').innerText = calculator.mult();
+  document.querySelector('.firstI').value = calculator.mult();
   document.querySelector('.secondI').value = '';
 })
 
